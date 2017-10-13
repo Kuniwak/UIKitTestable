661a6a4e37d52c51415c801d4fb5df37d755e987
Kuniwak <orga.chem.job@gmail.com>
2017-10-13 15:26:03 +0900

    Initial commit

---
+++ UIKitTestable/UIKitTestable.h
@@ -0,0 +1,19 @@
+//
+//  UIKitTestable.h
+//  UIKitTestable
+//
+//  Created by yuki.kokubun on 2017/10/13.
+//  Copyright © 2017年 kuniwak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitTestable.
+FOUNDATION_EXPORT double UIKitTestableVersionNumber;
+
+//! Project version string for UIKitTestable.
+FOUNDATION_EXPORT const unsigned char UIKitTestableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitTestable/PublicHeader.h>
+
+
