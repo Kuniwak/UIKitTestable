6a79a3d8c25bd34b6261f9689539149b256af963
Kuniwak <orga.chem.job@gmail.com>
2018-09-30 23:39:13 +0900

    Create the new target "UITests"

---
+++ UIKitTests/UIKitTests/UIKitTests.h
@@ -0,0 +1,19 @@
+//
+//  UIKitTests.h
+//  UIKitTests
+//
+//  Created by Yuki Kokubun on 2018/09/30.
+//  Copyright © 2018年 Yuki Kokubun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitTests.
+FOUNDATION_EXPORT double UIKitTestsVersionNumber;
+
+//! Project version string for UIKitTests.
+FOUNDATION_EXPORT const unsigned char UIKitTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitTests/PublicHeader.h>
+
+
