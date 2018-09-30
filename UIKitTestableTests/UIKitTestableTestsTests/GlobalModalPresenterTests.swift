9dc757e66e1a26d8b70515f15fca0957f47380f9
Kuniwak <orga.chem.job@gmail.com>
2018-10-01 02:34:49 +0900

    Modify memory leak tests

--- UIKitTestableTests/UIKitTestableTestsTests/GlobalModalPresenterTests.swift
+++ UIKitTestableTests/UIKitTestableTestsTests/GlobalModalPresenterTests.swift
@@ -59,27 +59,56 @@ class GlobalModalPresenterTests: XCTestCase {
         // NOTE: CPS is a simple way instead of Promisified tests in this case,
         // because this test target (UIViewController#dismiss) is designed for CPS.
         let expectation = self.expectation(description: "Awaiting a call of viewDidDisappear")
+        let thresholdOfAcceptableAliveWindows: UInt = 5
 
-        let viewController = UIViewController()
+        var createdWindowLogs: [String] = ["↑ Older UIWindow"]
 
-        let modalPresenter = GlobalModalPresenter(wherePresentOn: CountedWindow())
-        modalPresenter.present(
-            viewController: viewController,
-            animated: false,
-            completion: {
-                DispatchQueue.main.async {
-                    modalPresenter.dismiss(
-                        animated: false,
-                        completion: {
-                            XCTAssertEqual(CountedWindow.numberOfLiving, 1, "UIWindow was not released")
-                            _ = modalPresenter
-                            expectation.fulfill()
-                        }
-                    )
-                }
+        repeatAsync(
+            count: 100,
+            repeating: { (i, done) in
+                let viewController = UIViewController()
+                let window = CountedWindow()
+                let modalPresenter = GlobalModalPresenter(wherePresentOn: window)
+                createdWindowLogs.append("\(i): \(window)")
+
+                modalPresenter.present(
+                    viewController: viewController,
+                    animated: false,
+                    completion: {
+                        modalPresenter.dismiss(
+                            animated: false,
+                            completion: {
+                                DispatchQueue.main.async {
+                                    done()
+                                }
+                            }
+                        )
+                    }
+                )
             }
-        )
+        ) {
+            var livingWindowsInfo = ""
+            dump(UIApplication.shared.windows, to: &livingWindowsInfo)
+            createdWindowLogs.append("↓ Newer UIWindow")
 
-        self.waitForExpectations(timeout: 3)
+            // XXX: Some UIWindows can be remained, but it is not a problem only if few remained.
+            XCTAssertLessThan(
+                CountedWindow.numberOfLiving, thresholdOfAcceptableAliveWindows,
+                "A lot of UIWindow have been not released:\n\(livingWindowsInfo)\n\n\(createdWindowLogs.joined(separator: "\n"))"
+            )
+
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 10)
     }
+}
+
+
+private func repeatAsync(count: UInt, repeating f: @escaping(UInt, @escaping () -> Void) -> Void, _ last: @escaping () -> Void) {
+    let first = (0..<count).reduce(last) { (prev: @escaping () -> Void, i: UInt) -> () -> Void in
+        return { f(i, prev) }
+    }
+
+    first()
 }
