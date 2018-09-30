52112c7db4bdea89ed62af9c63191f8980fd1d29
Kuniwak <orga.chem.job@gmail.com>
2018-09-30 14:35:18 +0900

    Create a new xcworkspace

---
+++ UIKitTestable/UIKitTestable/UIKitTestable.h
@@ -0,0 +1,19 @@
+//
+//  UIKitTestable.h
+//  UIKitTestable
+//
+//  Created by Yuki Kokubun on 2018/09/30.
+//  Copyright © 2018年 Yuki Kokubun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitTestable.
+FOUNDATION_EXPORT double UIKitTestableVersionNumber;
+
+//! Project version string for UIKitTestable.
+FOUNDATION_EXPORT const unsigned char UIKitTestableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitTestable/PublicHeader.h>
+
+
---
+++ UIKitTestableTests/UIKitTestableTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitTestableTests
+//
+//  Created by Yuki Kokubun on 2018/09/30.
+//  Copyright © 2018年 Yuki Kokubun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
