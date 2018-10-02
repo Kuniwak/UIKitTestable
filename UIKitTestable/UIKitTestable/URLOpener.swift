a6cb288a8694bc7cf7cd230ef80e0efb5b14e4c4
Kuniwak <orga.chem.job@gmail.com>
2018-10-02 20:19:12 +0900

    Support completions for `UIApplication#open` for iOS 10.0+

--- UIKitTestable/UIKitTestable/URLOpener.swift
+++ UIKitTestable/UIKitTestable/URLOpener.swift
@@ -8,11 +8,26 @@ import UIKit
 public protocol URLOpenerProtocol {
     /**
      Attempts to open the resource at the specified URL.
-     This method behave like `UIApplication#open`.
+     This method behave like `UIApplication#openURL`.
      */
     func open(url: URL)
-}
 
+
+    /**
+     Attempts to open the resource at the specified URL.
+     This method behave like `UIApplication#open`.
+     */
+    @available(iOS 10.0, *)
+    func open(url: URL, completion: @escaping (Bool) -> Void)
+
+
+    /**
+     Attempts to open the resource at the specified URL.
+     This method behave like `UIApplication#open`.
+     */
+    @available(iOS 10.0, *)
+    func open(url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completion: @escaping (Bool) -> Void)
+}
 
 
 /**
@@ -21,15 +36,27 @@ public protocol URLOpenerProtocol {
  */
 public final class ApplicationURLOpener: URLOpenerProtocol {
     public func open(url: URL) {
-        if #available (iOS 10.0, *) {
+        if #available(iOS 10.0, *) {
             // NOTE: For iOS 10.0+.
             // https://developer.apple.com/reference/uikit/uiapplication/1648685-open
-            UIApplication.shared.open(url)
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
         else {
             // NOTE: For iOS 2.0–10.0.
             // https://developer.apple.com/reference/uikit/uiapplication/1622961-openurl#parameters
             UIApplication.shared.openURL(url)
         }
+    }
+
+
+    @available(iOS 10.0, *)
+    public func open(url: URL, completion: @escaping (Bool) -> Void) {
+        UIApplication.shared.open(url, options: [:], completionHandler: completion)
+    }
+
+
+    @available(iOS 10.0, *)
+    public func open(url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completion: @escaping (Bool) -> Void) {
+        UIApplication.shared.open(url, options: options, completionHandler: completion)
     }
 }
