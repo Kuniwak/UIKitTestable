7b40a6b796393df4b172a2cdcc254087156d0ada
Kuniwak <orga.chem.job@gmail.com>
2018-10-19 00:42:14 +0900

    Split to MemoryLeakTestKit

--- UIKitTests/UIKitTests/Arrays.swift
+++ UIKitTests/UIKitTests/Arrays.swift
@@ -1,27 +1,4 @@
-internal func scan<T, R, S: Sequence>(
-    _ xs: S,
-    initial: R,
-    _ f: (R, T) -> R
-) -> [R] where S.Element == T {
-    var intermediate = initial
-
-    return xs.map { x in
-        intermediate = f(intermediate, x)
-        return intermediate
-    }
-}
-
-
-
-internal func slide<T, S: Sequence>(
-    _ xs: S
-) -> [(T, T)] where S.Element == T {
-    return Array(zip(xs, xs.dropLast()))
-}
-
-
-
-internal func intersperse<T, CollectionT: Collection>(
+public func intersperse<T, CollectionT: Collection>(
     _ xs: CollectionT,
     _ y: T
 ) -> [T] where CollectionT.Element == T {
